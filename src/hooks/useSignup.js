6d5dc0b3036b9a45d70f45f11ebde3c33c42f618
chubinashvili <chubinashvili2003@gmail.com>
2022-11-29 21:28:55 +0400

    added creating user documents functionality to the useSignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth, projectStorage } from '../firebase/config'
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -9,8 +9,8 @@ export const useSignup = () => {
   const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName, thumbnail) => {
-    setError(null)
-    setIsPending(true)
+    setError(null);
+    setIsPending(true);
   
     try {
       // signup
@@ -27,6 +27,13 @@ export const useSignup = () => {
 
       // add display name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl });
+
+      // create a user document
+      await projectFirestore.collection('users').doc(res.user.uid).set({ 
+        online: true,
+        displayName,  
+        photoURL: imgUrl,
+      });
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user });
