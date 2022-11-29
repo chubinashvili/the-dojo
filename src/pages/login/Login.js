ae92974dfec3c64ea7eae1ed4e658a3dbbf8b8ea
chubinashvili <chubinashvili2003@gmail.com>
2022-11-29 21:58:19 +0400

    created a login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+
 // styles
 import './Login.css';
 
-const Login = () => {
+const Login = () => { 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }
   return (
-    <div>Login</div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>login</button>
+      {/* <button className='btn' disabled={isPending}>{isPending ? 'loading' : 'Login'}</button>
+      {error && <div className='error'>{error}</div>} */}
+    </form>
   )
 }
 
