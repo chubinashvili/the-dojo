4e045c73590e7852ab8d567aa6057b24625744b7
chubinashvili <chubinashvili2003@gmail.com>
2022-11-29 21:50:02 +0400

    added logout to the navbar and added useLogout hook to update user's online status

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { useLogout } from '../../hooks/useLogout';
 
 // styles & images
 import './Navbar.css';
 import Temple from '../../assets/temple.svg';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+  const { logout, isPending } = useLogout();
   return (
     <Fragment>
         <div className='navbar'>
@@ -14,16 +18,20 @@ const Navbar = () => {
                     <img src={Temple} alt="dojo logo" />
                     <span>The Dojo</span>
                 </li>
-
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Signup</Link>
-                </li>
-                <li>
-                    <button className='btn'>Logout</button>
-                </li>
+                {user ? (
+                    <li>
+                        <button className='btn' disabled={isPending} onClick={logout}>{isPending ? 'Logging out' : 'Logout'}</button>
+                    </li>
+                ) : (
+                    <Fragment>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">Signup</Link>
+                        </li>
+                    </Fragment>
+                )}
             </ul>
         </div>
         <Outlet />
