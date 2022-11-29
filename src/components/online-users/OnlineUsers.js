8df3315599004953cbc0162f076a5f45e08a5676
chubinashvili <chubinashvili2003@gmail.com>
2022-11-30 00:07:17 +0400

    showing online user

--- src/components/online-users/OnlineUsers.js
+++ src/components/online-users/OnlineUsers.js
@@ -7,13 +7,14 @@ import Avatar from '../avatar/Avatar';
 import './OnlineUsers.css';
 
 const OnlineUsers = () => {
-  const { error, documents } = useCollection('users')
+  const { error, documents } = useCollection('users');
   return (
     <div className='user-list'>
         <h2>All Users</h2>
         {error && <div className='error'>{error}</div>}
         {documents && documents.map(user => (
             <div key={user.id} className='user-list-item'>
+                {user.online && <span className='online-user'></span>}
                 <span>{user.displayName}</span>
                 <Avatar src={user.photoURL} />
             </div>
