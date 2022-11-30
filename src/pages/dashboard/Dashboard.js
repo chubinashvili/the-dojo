103df60ba1cb3c0c128452f67dd4e9d721976735
chubinashvili <chubinashvili2003@gmail.com>
2022-11-30 17:22:30 +0400

    added fething all the projects to the Dashboard page and displaying it using ProjectList component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,18 @@
+import ProjectList from '../../components/project-list/ProjectList';
+import { useCollection } from '../../hooks/useCollection';
+
 // styles
 import './Dashboard.css';
 
 const Dashboard = () => {
+  const { documents, error } = useCollection('projects');
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
   )
 }
 
