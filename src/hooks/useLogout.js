9c9a93bc36ec93250b726867343a930ffc108a5d
chubinashvili <chubinashvili2003@gmail.com>
2022-12-01 18:07:59 +0400

    removed useNavigate redundant import inside useLogout hook

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { projectAuth, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
